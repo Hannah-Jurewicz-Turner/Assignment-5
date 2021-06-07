dd4e14a71b488e617639d1f9b8c1d54873946ca7
HannahJT <>
2021-06-07 16:06:55 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// calculates the area of the circle with the radius as well as the circumference.
+
+document.getElementById('button-one').addEventListener('click', calculateAreaOfCircle)
+
+function calculateAreaOfCircle () {
+  let intRadius = parseInt(document.getElementById ('input').value)
+  intRadius = intRadius **2
+  intRadius = intRadius *3.14159265359
+  alert(intRadius)
+}
+
+document.getElementById('button-two').addEventListener('click', calculateCircumferenceOfCircle)
+
+function calculateCircumferenceOfCircle () {
+  let intRadius = parseInt(document.getElementById('input').value)
+  intRadius = intRadius *6.28318530718
+  alert(intRadius)
+}
