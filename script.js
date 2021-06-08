512ef90dc1f8bd3a6ba2ab8f10de503f8bf7cae0
HannahJT <>
2021-06-08 14:45:39 +0000

    Added more text and made the answer a paragraph instead of alert.

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-// calculates the area of the circle with the radius as well as the circumference.
+// connects the first button with the 'area of a circle' function
+let intRadius = 0
 
 document.getElementById('button-one').addEventListener('click', calculateAreaOfCircle)
 
 function calculateAreaOfCircle () {
-  let intRadius = parseInt(document.getElementById('input').value)
+  // allows text to appear with the area value when user inputs the radius and clicks the button
+  intRadius = parseInt(document.getElementById('input').value)
   intRadius = intRadius ** 2
   intRadius = intRadius * 3.14159265359
-  alert(intRadius)
+  document.getElementById('answer').innerHTML = 'The area of the circle is ' + intRadius
 }
 
+// connects the second button to the function 'calculate circumference of circle'
 document.getElementById('button-two').addEventListener('click', calculateCircumferenceOfCircle)
 
 function calculateCircumferenceOfCircle () {
-  let intRadius = parseInt(document.getElementById('input').value)
+  // allows the answer to appear with the circumference value when user inputs their radius.
+  intRadius = parseInt(document.getElementById('input').value)
   intRadius = intRadius * 6.28318530718
-  alert(intRadius)
+  document.getElementById('answer').innerHTML = 'The circumference of the circle is ' + intRadius
 }
